d4d25db10743b4d081fd4a29a24e853a4750571f
Sajal Roy <sajal2020roy@gmail.com>
2025-01-12 16:52:53 +0600

    add from local

---
+++ public/admin/js/student.js
@@ -0,0 +1,29 @@
+(function ($) {
+    "use strict";
+
+    $("#studentDataTable").DataTable({
+        pageLength: 10,
+        ordering: false,
+        serverSide: true,
+        processing: true,
+        searching: true,
+        ajax: $('#student-list-route').val(),
+        language: {
+			paginate: {
+				previous: "<i class='fa-solid fa-angles-left'></i>",
+				next: "<i class='fa-solid fa-angles-right'></i>",
+			},
+			searchPlaceholder: "Search event category",
+			search: "<span class='searchIcon'><i class='fa-solid fa-magnifying-glass'></i></span>",
+		},
+		dom: '<"tableTop"<"row align-items-center"<"col-sm-6"<"tableSearch float-start"f>><"col-sm-6"<"tableLengthInput float-end"l>>>>tr<"tableBottom"<"row align-items-center"<"col-sm-6"<"tableInfo"i>><"col-sm-6"<"tablePagi"p>>>><"clear">',
+		columns: [
+            {"data": "name", "name": "name"},
+            {"data": "email", "name": "email"},
+            {"data": "city", "name": "city"},
+            {"data": "address", "name": "address"},
+            {"data": "action", searchable: false, responsivePriority:2},
+        ],
+      });
+
+})(jQuery)
